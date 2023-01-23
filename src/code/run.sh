26ed4df2f4d06a27845a3f10d7af9a591018ea58
Conall Kavanagh <conall.kavanagh76@mail.dcu.ie>
2023-01-23 14:14:33 +0000

    made a run script and modified a few tests

---
+++ src/code/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+java -Xmx500M -cp "/usr/local/lib/antlr-4.11.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool placeholder.g4 
+javac *.java 
+java -Xmx500M -cp "/usr/local/lib/antlr-4.11.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig placeholder prog test.txt -tokens
+
