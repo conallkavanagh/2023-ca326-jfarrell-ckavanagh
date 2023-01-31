643edd2a6b881dd765c88128e7d3cc5385716683
Conall Kavanagh <conall.kavanagh76@mail.dcu.ie>
2023-01-31 22:26:32 +0000

    got it working with the visitor pattern

--- src/code/placeholderDisplayVisitor.java
+++ src/code/placeholderDisplayVisitor.java
@@ -1,22 +1,22 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class placeholderDisplayVisitor extends placeholderBaseVisitor<Integer> {
+public class placeholderDisplayVisitor extends placeholderBaseVisitor<Float> {
 
-    Map<String, Integer> memory = new HashMap<String, Integer>();
+    Map<String, Float> memory = new HashMap<String, Float>();
 
     @Override
-    public Integer visitAssignstmt(placeholderParser.AssignstmtContext ctx) {
-        String id = ctx.ID().getText();  // id is left-hand side of '='
-        int value = Integer.valueOf(ctx.expression().getText());   // compute value of expression on right
-        memory.put(id, value);           // store it in our memory
-        System.out.format("%s = %d", id, value);
+    public Float visitAssignstmt(placeholderParser.AssignstmtContext ctx) {
+        String id = ctx.ID().getText();          // id is left-hand side of '='
+        float value = visit(ctx.expression());   // compute value of expression on right
+        memory.put(id, value);                   // store it in our memory
+        System.out.format("%s = %f", id, value);
         return value;
     }
 
-    // /** INT */
-    // @Override
-    // public Integer visit(LabeledExprParser.IntContext ctx) {
-    //     return Integer.valueOf(ctx.INT().getText());
-    // }
+    /** NUMBER */
+    @Override
+    public Float visitTerms(placeholderParser.TermsContext ctx) {
+        return Float.valueOf(ctx.term().getText());
+    }
 }
